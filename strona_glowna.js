73d418e8eca05913006a8a7e1eabf01ddbe4025c
Katolik4 <piotr.polakowski92@gmail.com>
2017-09-17 18:32:18 +0200

    V1.2

---
+++ strona_glowna.js
@@ -0,0 +1,10 @@
+function wyswietl_okno()
+{
+	document.getElementById("test_okno").innerHTML="afhadfhsdfhdsfhsdfhsdfh";
+}
+
+function fokus_1()
+{
+	document.getElementByID("test_okno").innerHTML="fokus!!";
+}
+
